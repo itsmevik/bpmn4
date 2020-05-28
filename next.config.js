fc9b936cc6ab21fc29bddc66f9788db0f98d8a95
Raviteja <ravi.bcid@gmail.com>
2020-05-28 14:23:59 +0530

    Added server.js file

--- next.config.js
+++ next.config.js
@@ -13,7 +13,8 @@ withCSS(
       AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
       REDIRECT_URI: process.env.REDIRECT_URI,
       POST_LOGOUT_REDIRECT_URI: process.env.POST_LOGOUT_REDIRECT_URI,
-      API_BASE_URI: process.env.API_BASE_URI
+      API_BASE_URI: process.env.API_BASE_URI,
+      PORT:process.env.PORT || 3000
     }
   })
 );
---
+++ server/index.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const { createProxyMiddleware } = require("http-proxy-middleware")
+const next = require("next")
+const nextCookie = require("next-cookies")
+const env = process.env.NODE_ENV
+const port=process.env.PORT || 3000
+const devMode = env !== "production"
+const app = next({
+  dev: devMode,
+})
+console.log(process.env.API_BASE_URI)
+const handle = app.getRequestHandler()
+
+let server
+app
+  .prepare()
+  .then(() => {
+    server = express()
+    
+    const devProxy = {
+      "/laravel-secure/*": createProxyMiddleware({
+        target: process.env.API_BASE_URI,
+        pathRewrite: (path, req) => path.replace("/laravel-secure", ""),
+        changeOrigin: true,
+        onProxyReq: (proxyReq, req, res) => {
+          const cookie = nextCookie({ req })
+          const { token } = cookie
+          proxyReq.setHeader("Authorization", `Bearer ${token}`)
+        },
+      }),
+      "/laravel/*": createProxyMiddleware({
+        target: process.env.API_BASE_URI,
+        pathRewrite: (path, req) => path.replace("/laravel", ""),
+        changeOrigin: true,
+      })
+    }
+
+    // Set up the proxy.
+    for (let context in devProxy) server.use(context, devProxy[context])
+
+    // Default catch-all handler to allow Next.js to handle all other routes
+    server.all("*", handle)
+
+    server.listen(port, error => {
+      if (error) throw error
+
+      console.log(`> Ready on port ${port} [${env}]`)
+    })
+  })
+  .catch(error => {
+    console.log("An error occurred, unable to start the server")
+    console.log(error)
+  })
