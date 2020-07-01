ddadb3707ebbf6275f040d593fa85b35b9910f7e
itsmevik <vikram@believecreative.com>
2020-07-01 11:42:47 +0000

    server file changes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const https = require('https');
+const fs = require('fs');
 const next = require("next");
 const nextCookie = require("next-cookies");
 const env = process.env.NODE_ENV;
@@ -54,12 +56,27 @@ app
 
       next();
     }
-
     server.listen(port, (error) => {
       if (error) throw error;
-
       console.log(`> Ready on port ${port} [${env}]`);
-    });
+    });0
+	// not required below code because we are proxying using vhost file.
+	/*if (devMode) {
+		server.listen(port, (err) => {
+		  if (err) throw err;
+		  console.log(`> Ready on port ${port} [${env}]`)
+		});
+	  } else {		
+		const key = fs.readFileSync('/etc/letsencrypt/live/bpmn4.com/privkey.pem');
+		const cert = fs.readFileSync('/etc/letsencrypt/live/bpmn4.com/fullchain.pem')
+		https.createServer({
+		  key,
+		  cert,
+		}, server).listen(port, (err) => {
+		  if (err) throw err;
+		  console.log(`> Ready on port ${port} [${env}]`)
+		});
+	  }*/
   })
   .catch((error) => {
     console.log("An error occurred, unable to start the server");
